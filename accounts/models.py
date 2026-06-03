ac0ece7f1bc44da230d3596390c2ea23e44d34a9
Shivam Kumar <shivam@Shivams-MacBook-Air.local>
2026-06-03 16:47:09 +0530

    added jwt auth + test view for testing

--- accounts/models.py
+++ accounts/models.py
@@ -40,7 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     
 
-    object = UserManager()
+    objects = UserManager()
 
     def __str__(self):
         return self.email
